ad39aad73bb309a1c1862a3d0c4fc5016078dbd6
loinlt <loinlt.stageid.app>
2021-03-15 17:18:28 +0700

    update for project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonsContainer } from './containers/PokemonsContainer';
+import './App.css';
+
+function App() {
+
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon2.vercel.app/'
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <PokemonsContainer />
+    </ApolloProvider>
+  );
+}
+
+export default App;
